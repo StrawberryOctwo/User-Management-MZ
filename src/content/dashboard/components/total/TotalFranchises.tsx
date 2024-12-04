df5bad455a08481e115d1a674f4b2510391c5851
unknown <jaffal250@gmail.com>
2024-12-04 23:39:04 +0200

    added pagination to the select franchise

--- src/content/dashboard/components/total/TotalFranchises.tsx
+++ src/content/dashboard/components/total/TotalFranchises.tsx
@@ -12,7 +12,8 @@ import {
   FormControl,
   InputLabel,
   MenuItem,
-  Select
+  Select,
+  CircularProgress
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import BusinessTwoToneIcon from '@mui/icons-material/BusinessTwoTone';
@@ -45,24 +46,35 @@ function TotalFranchises() {
   const [totalFranchises, setTotalFranchises] = useState(0);
   const [franchises, setFranchises] = useState([]);
   const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(15);
+  const [loading, setLoading] = useState(false);
 
   const handleFranchiseChange = (event) => {
     setSelectedFranchise(event.target.value);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      // Fetch all franchises
-      const data = await fetchFranchises(page, limit);
-      console.log(data);
-      // Set totalFranchises to the length of the data
-      setFranchises(data.data);
-      setTotalFranchises(data.total);
-    };
+  const fetchMoreFranchises = async () => {
+    setLoading(true);
+    const data = await fetchFranchises(page, limit);
+    setFranchises((prevFranchises) => [...prevFranchises, ...data.data]);
+    setTotalFranchises(data.total);
+    setLoading(false);
+  };
 
-    fetchData();
-  }, []);
+  useEffect(() => {
+    fetchMoreFranchises();
+  }, [page]);
+
+  const handleScroll = (event) => {
+    console.log('Scroll event detected');
+    const bottom =
+      event.target.scrollHeight - event.target.scrollTop ===
+      event.target.clientHeight;
+    if (bottom && !loading) {
+      console.log('Reached bottom, loading more...');
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
 
   return (
     <Card>
@@ -70,17 +82,40 @@ function TotalFranchises() {
         {/* <Typography variant="overline" color="text.primary">
           {t('Franchises')}
         </Typography> */}
-        <FormControl
-          variant="outlined"
-          sx={{ minWidth: '150px', maxHeight: '50px' }}
-        >
-          <Select value={selectedFranchise} onChange={handleFranchiseChange}>
+        <FormControl variant="outlined" sx={{ minWidth: 120 }}>
+          <Select
+            value={selectedFranchise}
+            onChange={handleFranchiseChange}
+            MenuProps={{
+              PaperProps: {
+                onScroll: handleScroll,
+                sx: {
+                  maxHeight: 500,
+                  '&::-webkit-scrollbar': {
+                    width: '8px'
+                  },
+                  '&::-webkit-scrollbar-thumb': {
+                    backgroundColor: '#888',
+                    borderRadius: '4px'
+                  },
+                  '&::-webkit-scrollbar-thumb:hover': {
+                    backgroundColor: '#555'
+                  }
+                }
+              }
+            }}
+          >
             <MenuItem value="All Franchises">All Franchises</MenuItem>
             {franchises.map((franchise) => (
               <MenuItem key={franchise.id} value={franchise.name}>
                 {franchise.name}
               </MenuItem>
             ))}
+            {loading && (
+              <MenuItem disabled>
+                <CircularProgress size={24} />
+              </MenuItem>
+            )}
           </Select>
         </FormControl>
 
